0618555837e460ac27f7dd3194a8e5c05a311575
Eric Faraut <eric.faraut@hotmail.fr>
2026-03-10 17:45:16 +0100

    AboutAsserts is done.

--- Koans/1_AboutAsserts.cs
+++ Koans/1_AboutAsserts.cs
@@ -9,22 +9,22 @@ public class AboutAsserts : Koan
 	[Step(1)]
 	public void AssertTruth()
 	{
-		Assert.True(false); //This should be true
+		Assert.True(true && "true" == "true" && 1 + 1 == 2);
 	}
 
 	//Enlightenment may be more easily achieved with appropriate messages
 	[Step(2)]
 	public void AssertTruthWithMessage()
 	{
-		Assert.True(false, "This should be true -- Please fix this");
+		Assert.True(true && "true" == "true" && 1 + 1 == 2, "This is true");
 	}
 
 	//To understand reality, we must compare our expectations against reality
 	[Step(3)]
 	public void AssertEquality()
 	{
-		var expectedValue = 3;
-		var actualValue = 1 + 1;
+		int expectedValue = 3;
+		int actualValue = 1 + 1 + 1;
 		Assert.True(expectedValue == actualValue);
 	}
 
@@ -32,8 +32,8 @@ public class AboutAsserts : Koan
 	[Step(4)]
 	public void ABetterWayOfAssertingEquality()
 	{
-		var expectedValue = 3;
-		var actualValue = 1 + 1;
+		int expectedValue = 3;
+		int actualValue = 1 + 1 + 1;
 		Assert.Equal(expectedValue, actualValue);
 	}
 
@@ -41,6 +41,6 @@ public class AboutAsserts : Koan
 	[Step(5)]
 	public void FillInValues()
 	{
-		Assert.Equal(FILL_ME_IN, 1 + 1);
+		Assert.Equal(2, 1 + 1);
 	}
 }
