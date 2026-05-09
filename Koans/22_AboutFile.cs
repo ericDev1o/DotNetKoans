79e01d64e15e9eff2e0e68afce1318b5fa013a31
Eric Faraut <eric.faraut@hotmail.fr>
2026-05-09 16:57:26 +0200

    AboutFile is solved.

--- Koans/22_AboutFile.cs
+++ Koans/22_AboutFile.cs
@@ -12,19 +12,20 @@ public class AboutFile : Koan
 	// and opening files, and helps in the creation of FileStream objects.
 
 	[Step(1)]
-	public void CreatingAndDeletingFile()
+	public static void CreatingAndDeletingFile()
 	{
-		string path = IOPath.GetTempFileName(); // GetTempFileName() Creates a uniquely named, zero-byte temporary file on disk and returns the full path of that file.
+		string path = IOPath.GetTempFileName(); 
+		// GetTempFileName() Creates a uniquely named, zero-byte temporary file on disk and returns the full path of that file.
             
-		Assert.Equal(FILL_ME_IN, File.Exists(path)); 
+		Assert.True(File.Exists(path)); 
 
 		File.Delete(path);
 
-		Assert.Equal(FILL_ME_IN, File.Exists(path));
+		Assert.False(File.Exists(path));
 	}
         
 	[Step(2)]
-	public void CopyFile()
+	public static void CopyFile()
 	{
 		string path = IOPath.GetTempFileName();
 		string newPath = IOPath.Combine(IOPath.GetTempPath(), "newFile.txt");
@@ -32,12 +33,12 @@ public class AboutFile : Koan
 		File.Delete(newPath);
 		File.Copy(path, newPath);
 
-		Assert.Equal(FILL_ME_IN, File.Exists(path));
-		Assert.Equal(FILL_ME_IN, File.Exists(newPath));
+		Assert.True(File.Exists(path));
+		Assert.True(File.Exists(newPath));
 	}
 
 	[Step(3)]
-	public void MoveFile()
+	public static void MoveFile()
 	{
 		string path = IOPath.GetTempFileName();
 		string newPath = IOPath.Combine(IOPath.GetTempPath(), "newFile.txt");
@@ -45,29 +46,28 @@ public class AboutFile : Koan
 		File.Delete(newPath);
 		File.Move(path, newPath);
 	        
-		Assert.Equal(FILL_ME_IN, File.Exists(path));
-		Assert.Equal(FILL_ME_IN, File.Exists(newPath));
+		Assert.False(File.Exists(path));
+		Assert.True(File.Exists(newPath));
 	}
         
 	[Step(4)]
-	public void GetFileInfo()
+	public static void GetFileInfo()
 	{
 		string path = IOPath.GetTempFileName();
-		FileInfo fileInfo = new FileInfo(path);
+		FileInfo fileInfo = new(path);
 
-		Assert.Equal(FILL_ME_IN, fileInfo.Exists);
-		Assert.Equal(FILL_ME_IN, fileInfo.FullName); // what is the file name?
+		Assert.True(fileInfo.Exists);
+		Assert.Equal(path, fileInfo.FullName); // what is the file name?
 	}
         
 	[Step(5)]
-	public void ReadFile()
+	public static void ReadFile()
 	{
-
 		string data = "Hello World!";
 		string path = createFileAndFillIn(data);
 
 		byte[] bytes = new byte[data.Length];
-		UTF8Encoding temp = new UTF8Encoding(true);
+		UTF8Encoding temp = new(true);
 		string readMessage = "";
 		using (FileStream fs = File.OpenRead(path))
 		{
@@ -76,23 +76,23 @@ public class AboutFile : Koan
 				readMessage = temp.GetString(bytes);
 			}
 		}
-		Assert.Equal(FILL_ME_IN, readMessage); // what is the message?
+		
+		Assert.Equal("Hello World!", readMessage); // what is the message?
 	}
 
 	[Step(6)]
-	public void ReadLines()
+	public static void ReadLines()
 	{
-
 		string data = "Line0\nLine1\nLine2";
 		string path = createFileAndFillIn(data);
 
 		var lines = File.ReadAllLines(path);
             
-		Assert.Equal(FILL_ME_IN, lines.Length); // what is the number of lines?
-		Assert.Equal(FILL_ME_IN, lines[1]); // what is written in the line No.2 ?
+		Assert.Equal(3, lines.Length); // what is the number of lines?
+		Assert.Equal("Line1", lines[1]); // what is written in the line No.2 ?
 	}
 
-	private string createFileAndFillIn(string data)
+	private static string createFileAndFillIn(string data)
 	{
 		string path = IOPath.GetTempFileName();
 		byte[] info = new UTF8Encoding(true).GetBytes(data);
